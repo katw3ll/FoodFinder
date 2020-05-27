50e3b55e8affb96a7a0995ca55a26af9744fd726
katw3ll <pkatalev@mail.ru>
2020-05-27 22:56:06 +0300

    PanoramaActivity finished

--- app/src/main/java/com/example/foodfinder/MainActivity.java
+++ app/src/main/java/com/example/foodfinder/MainActivity.java
@@ -21,6 +21,14 @@ import android.view.WindowManager;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.foodfinder.Model.PlacePoint;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -49,6 +57,9 @@ import com.yandex.mapkit.search.search_layer.PlacemarkListener;
 import com.yandex.mapkit.search.search_layer.SearchResultItem;
 import com.yandex.runtime.image.ImageProvider;
 
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -78,15 +89,21 @@ public class MainActivity extends AppCompatActivity implements ClusterListener {
 
     int iconId = R.drawable.ic_burger;
 
-    private APIHandler api;
+    private String apiUrl = "https://apidata.mos.ru/v1/datasets/1903/rows?$top=5000";
+    private String versionUrl = "https://apidata.mos.ru/v1/datasets/1903/version";
+    private String apiKey = "api_key=dc1e343a94767732736a858f05dd4a98";
 
+    private RequestQueue mQueue;
 
-
+    private String versionPathName = "version.json";
+    private String dataFileName = "data.json";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        mQueue = Volley.newRequestQueue(this);
         try {
-            api = new APIHandler(this);
+            checkVersion();
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -109,18 +126,9 @@ public class MainActivity extends AppCompatActivity implements ClusterListener {
         clusterizedCollection =
                 mapview.getMap().getMapObjects().addClusterizedPlacemarkCollection(this);
 
-//        setPointsOnMap(restaurantList, iconId);
-
         ChipNavigationBar menu = findViewById(R.id.navigation);
-
         menu.setOnItemSelectedListener(mOnNavigationItemSelectedListener);
-
         menu.setItemSelected(R.id.navigation_restaurant, true);
-
-
-
-
-
     }
 
     private void getPointsFromFile(){
@@ -235,10 +243,7 @@ public class MainActivity extends AppCompatActivity implements ClusterListener {
             return true;
         }
     };
-
-
-
-
+    
     @Override
     protected void onStop() {
         super.onStop();
@@ -348,6 +353,94 @@ public class MainActivity extends AppCompatActivity implements ClusterListener {
             this.text = text;
             this.imgId = imgId;
         }
+    }
+
+
+    private void checkVersion() throws IOException {
+        String url = versionUrl + "?" + apiKey;
+
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        String currentVersion = response.toString();
+                        String version = "";
+                        try {
+                            BufferedReader br = new BufferedReader(new InputStreamReader(
+                                    openFileInput(versionPathName)));
+                            version = br.readLine();
+                        } catch (IOException e) {
+                            saveVersion(currentVersion);
+                            e.printStackTrace();
+                        }
+                        System.out.println(currentVersion);
+                        System.out.println(version);
+                        if (!currentVersion.equals(version)){
+                            saveVersion(currentVersion);
+                            getNewData();
+                        }else{
+                            System.out.println("Файл не обновлен");
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        });
+
+        mQueue.add(request);
+
+    }
+
+    private void saveVersion(String version) {
+        FileOutputStream outputStream = null;
+        try {
+            outputStream = openFileOutput(versionPathName, Context.MODE_PRIVATE);
+            outputStream.write(version.getBytes());
+            outputStream.flush();
+            outputStream.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void getNewData() {
+        System.out.println("Получаем файл");
+        String url = apiUrl + "&" + apiKey;
+
+        StringRequest request = new StringRequest (Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            BufferedWriter bw =  new BufferedWriter(new OutputStreamWriter(
+                                    openFileOutput(dataFileName, MODE_PRIVATE)));
+                            bw.write(response);
+                            System.out.println("Записан файл");
+                            // закрываем поток
+                            bw.close();
+                        } catch (FileNotFoundException e) {
+                            e.printStackTrace();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        // пишем данные
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        });
+        request.setRetryPolicy(new DefaultRetryPolicy(
+                999999999,
+                DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+                DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+        mQueue.add(request);
     }
 
 }
